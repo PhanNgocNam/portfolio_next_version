1f276990635873dc1d7d3312d59c22ee60bc6461
Nam <realcm471@gmail.com>
2023-12-29 13:42:48 +0700

    Add footer components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Intro from "@/components/Intro";
 import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDevider";
@@ -15,6 +16,7 @@ export default function Home() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
       {/* <Experience /> */}
     </main>
   );
